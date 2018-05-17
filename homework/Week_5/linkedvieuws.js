b1098cafcc7fac3f3171c2c0e66b9aebdf06b05a
Marieke van Maanen <mariekehvanmaanen@gmail,com>
2018-05-17 10:37:32 +0200

    bar update

--- homework/Week_5/linkedvieuws.js
+++ homework/Week_5/linkedvieuws.js
@@ -9,117 +9,56 @@ Creates a map of Africa
 **/
 
 window.onload = function() {
+/**
+	Op deze manier comments
+**/
 	d3.select("head").append("title")
-						.text("Total Slave Exports Between 1400-1900");
+						.text("Total Slave Exports between 1400-1900");
 
-	// d3.select("body").append("h1")
-	// 						.text("Total Slave Exports Between 1400-1900");
-
-	// d3.select("body").append("h3")
-	// 						.text("Marieke van Maanen (10888640)");	
-		GetData();
+		getData();
+		//makeLegend();
+		//promeces for js of iets doen om  asynchroniteit te krijgen
+		// aan Kim vragen hoe ik dit moet doen
+		//makeMap();
 
 };
 
-function GetData(error) {
+function getData(error) {
 	if (error) throw error;
 	
 	d3.queue()
-  	.defer(d3.json, "exports.json")
-  	.defer(d3.json, "only_total_exports.json")
-  	.awaitAll(MakeMap);
+  	.defer(d3.json, "exports_per_country.json")
+  	.defer(d3.json, "total_exports_per_country.json")
+  	.defer(d3.json, "exports_africa.json")
+  	.awaitAll(function (error, response) {
+  		if (error) throw error;
+
+  		makeMap(response);
+  		makeBar(response);
+  	});	
 
 };
 
 
-function MakeMap(error, response) {
-	if (error) throw error;
+function makeMap(response) {
 
-	// log trans atlantic axports from Angola
-	console.log(typeof(response[0]["exports"][0].Angola.trans_atlantic))
-	console.log(response[1]["total_exports"])
+	var exports_per_country = response[1]["total_exports"];
+	console.log(response)
 
-	// devide countries and values
-    var total_slave_trades = [];
-    var countries = [];
-    
-    for (i = 0; i < 51; i++) {
-
-	    // select country names
-	    countries.push(Object.keys(response[1]["total_exports"][i]));
-
-	    // select all slave trades per country
-	    total_slave_trades.push(Object.values(response[1]["total_exports"][i]));
-
-	};
-
-
-//console.log(typeof(total_slave_trades[0]))
-console.log(total_slave_trades)
-	total_slave_trades.forEach(function(d) {
-		d = +d
-	});
-//console.log(typeof(total_slave_trades))
-
-    // initialize tooltip
-    var tooltip = d3.select("#container")
-        .append("div")
-        .attr("class", "tooltip hidden");
-
-        // define d3 tooltip			
-	var tip = d3.tip()
-			  .attr('class', 'd3-tip')
-			  .offset([-10, 0])
-			  .html(function(d) {
-			    return "<span style='color:red'>" + d + "</span><strong style='color:red'> Slave Trades </strong> ";
-			  });
-	
-	// call tooltip
-
-
-    // get the minimum and maximum exports
-    var max_slave_trades = d3.max(total_slave_trades, function(d) { return d; });
-console.log(max_slave_trades) // max is 94.633 but should be 3 million..
-    var min_slave_trades = d3.min(total_slave_trades, function(d) { return d; });
-
-	// color scale for map
-    var color = d3.scale.ordinal()
-    	.domain([min_slave_trades, max_slave_trades])
-        .range(["#feedde", "#fdd0a2", "#fdae6b", "#fd8d3c", "#e6550d", "#a63603"]);
-
-
-/*	.forEach(function(d){ 
-	var low=Math.round(100*Math.random()), 
-		mid=Math.round(100*Math.random()), 
-		high=Math.round(100*Math.random());
-	sampleData[d]={low:d3.min([low,mid,high]), high:d3.max([low,mid,high]), 
-			avg:Math.round((low+mid+high)/3), color:d3.interpolate("#ffffcc", "#800026")(low/100)};
-
-	// devide data in different groups
-	var colorScale = function(d) {
-	if (d == 0) {
-		return "0"
-	}
-	else if (d >= 1 && d < 1000) {
-		return "1-1,000"
-	}
-	else if (d >= 1000 && d < 10000) {
-		return "1,000-10,000"
-	}
-	else if (d >= 10000 && d < 100000) {
-		return "10,000-100,000"
-	}
-	else if (d >= 100000 && d < 1000000) {
-		return "100,000-1 million"
-	}
-	else if (d < 100000) {
-		return "1 million +"
-	};
-*/
     var map = new Datamap({
-			element: document.getElementById('container'),
+			element: document.getElementById('container_map'),
 			scope: "world",
+			data: exports_per_country,
 			
+			done: function(datamap) {
+	            datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+	                var location = geography.id;
+	                d3.selectAll("#plotTitle")
+	                					.text("Slave Exports in " + geography.properties.name + " (1400-1900)")
+	                update(location, response);
+	            });
+        	},
+
 			// zoom in on Africa
 			setProjection: function(element) {
 				var projection = d3.geo.equirectangular()
@@ -133,57 +72,56 @@ console.log(max_slave_trades) // max is 94.633 but should be 3 million..
 
 				return {path: path, projection: projection};
 			},
+
 			fills: {
-				defaultFill: "#a63603"
+				Zero: "#fdd0a2", 
+				Lower: "#fdae6b",
+				Low: "#fd8d3c",
+				Medium: "#f16913",
+				High: "#d94801",
+				Highest: "#8c2d04",
+				defaultFill: "whitesmoke"
 			},
+
 			geographyConfig: {
 				borderColor: "lightgrey",
 				popupOnHover: true,
 				highlightOnHover: true,
-				highlightFillColor: "darkgoldenrod"
-			// 	"popupTemplate":  function(geo, countries) {
-			//     		return '<div class=hoverinfo><strong>',
-			//     		+ geography.properties.name + 
-			//       		': ' + countries + '</strong></div>';
-			// }
+				highlightFillColor: "darkslategrey",
+				highlightBorderColor: "black",
+				popupTemplate: function(geo, data) {
+	                if (!data) {
+	                	return ['<div class="hoverinfo"><strong>',
+	                        'Country: </strong>' + geo.properties.name,
+	                        '</br>' + '<strong>No Data of Total Exports</strong>',
+	                        '</div>'].join('');
+	                }
+
+	                	return ['<div class="hoverinfo"><strong>',
+	                        'Country: </strong>' + geo.properties.name,
+	                        '</br>' + '<strong>Total exports: </strong>',
+	                        data.exports + '</div>'].join('');
+	            }
 		}
 	});
-
-
-
-    MakeBar(response)
 };
 
-function MakeBar (dataset) {
 
-	//console.log(dataset)
-	var slave_exports = dataset["0"]["exports"];
-	//console.log(slave_exports)
-	var angola = slave_exports["0"]["Angola"]
-	var zimbabwe = slave_exports["36"]["Zimbabwe"]
-	var burundi = slave_exports["38"]["Burundi"]
-	console.log(burundi)
+function makeBar (dataset) {
 
-	// does not work
-	slave_exports.forEach(function(d) {
-		d.indian_ocean = +d.indian_ocean;
-		d.red_sea = +d.red_sea;
-		d.trans_atlantic = +d.trans_atlantic
-		d.trans_saharan = +d.trans_saharan;
-	});
-	//console.log(typeof(sudan))
-	//console.log(typeof(burundi.trans_atlantic))
+	var total_exports = Object.values(dataset["2"]["0"]);
 
 	
 	// set dimensions for bargraph
-	var margin = {top: 40, right: 30, bottom: 60, left: 40},
+	var margin = {top: 70, right: 30, bottom: 60, left: 100},
     	width = 560 - margin.left - margin.right,
     	height = 500 - margin.top - margin.bottom,
 		barPadding = 5,
 		padding = 25;
 
 	// get svg element
-	var svg = d3.select("#container").append("svg")
+	var svg = d3.select("#container_bar")
+				.append("svg")
 				.attr("width", width + margin.left + margin.right)
 				.attr("height", height + margin.top + margin.bottom)
 				.append("g")
@@ -192,23 +130,27 @@ function MakeBar (dataset) {
     // define d3 tooltip			
 	var tip = d3.tip()
 			  .attr('class', 'd3-tip')
-			  .offset([-10, 0])
+			  .offset([-5, 0])
 			  .html(function(d) {
-			    return "<span style='color:red'>" + d + "</span><strong style='color:red'> Slave Trades </strong> ";
+			    return "<strong>" + d + "</strong> Exports";
 			  });
 	
 	// call tooltip on svg
 	svg.call(tip);
-
 
 	// create scale for x and y-axis
 	var x = d3.scale.ordinal()
 					.domain(["Trans-Atlanctic", "Indian Ocean", "Trans-Saharan", "Red Sea"])
 					.rangeRoundBands([0, width]);
 
+/*	var y = d3.scale.log()
+					.domain([1, d3.max(total_exports, function(d) { return d; })])
+					.range([height, 0]); */
+
 	var y = d3.scale.linear()
-					.domain([0, d3.max(Object.values(zimbabwe), function(d) { return d; })])
+					.domain([0, d3.max(total_exports, function(d) { return d; })])
 					.range([height, 0]); 
+
 
 	// define x and y-axis
     var xAxis = d3.svg.axis()
@@ -234,33 +176,127 @@ function MakeBar (dataset) {
 
 	var draw_y = svg.append("g")
 					    .attr("class", "axis")
+					    .attr("id", "y_axis")
 					    .call(yAxis)
 					.append("text")
 						.attr("transform", "rotate(-90)")
-					    .attr("y", 0 - margin.left)
+					    .attr("y", - 80)
 					    .attr("dy", ".8em")
 					    .style("text-anchor", "end")
-					    .text("Number of Slave Trades")
+					    .text("Number of Slave Exports")
             			.style("font-size", "13px")
             			.style("font-weight", "bold");
 
-    // create rectangles       			
+   // create rectangles       			
 	var rects = svg.selectAll(".bar")
-				    .data(Object.values(zimbabwe))
+				    .data(total_exports)
 					.enter()
 					.append("rect")
 					.attr("class", "bar")
-					.attr("x", function(d, i) { return i * (width / 4); })
+					.attr("x", function(d, i) { return i * (width / 4) + barPadding; })
 				    .attr("width", width / 4 - barPadding )
 				    .attr("y", function(d) { return y(d); })
 				    .attr("height", function(d) { return height - y(d); })
 				 	.on("mouseover", tip.show)
 					.on("mouseout", tip.hide);
-
-	// create plot title
+    
+	// add plot title				
     svg.append("text")
-       .attr("id", "PlotTitle")
-       .attr("y", -20)
-       .attr("x", width / 4)
-       .text("Total Slave Exports Between 1400-1900 (in zimbabwe..)")
+       .attr("id", "plotTitle")
+       .attr("y", -30)
+       .attr("x", width / 2)
+       .text("Total Slave Exports between 1400-1900")
+       .style("font-size", "17px")
+       .style("text-anchor", "middle");
+
+	//var color = d3.scale.ordinal(["#feedde", "#fdd0a2", "#fdae6b", "#fd8d3c", "#e6550d", "#a63603"])
 };
+
+
+
+
+
+
+function update(location, dataset) {
+/**
+	Update data section.
+**/
+
+	// set dimensions for bargraph
+	var margin = {top: 70, right: 30, bottom: 60, left: 100},
+    	width = 560 - margin.left - margin.right,
+    	height = 500 - margin.top - margin.bottom,
+		barPadding = 5,
+		padding = 25;
+
+	var slave_exports = dataset["0"]["exports"];
+
+    // define d3 tooltip			
+	var tip = d3.tip()
+			  .attr('class', 'd3-tip')
+			  .offset([-5, 0])
+			  .html(function(d) {
+			    return "<strong>" + d + "</strong> Exports";
+			  });
+	
+	var svg = d3.select("#container_bar")
+				.select("svg");
+
+	// call tooltip on svg
+	svg.call(tip);
+
+	var current_country = Object.values(slave_exports[location]);
+
+	// create scale for x and y-axis
+	var x = d3.scale.ordinal()
+					.domain(["Trans-Atlanctic", "Indian Ocean", "Trans-Saharan", "Red Sea"])
+					.rangeRoundBands([margin.left, width]);
+
+/*	var y = d3.scale.log()
+					.domain([1, d3.max(current_country, function(d) { return d; })])
+					.range([height, 0]); */
+
+	var y = d3.scale.linear()
+					.domain([0, d3.max(current_country, function(d) { return d; })])
+					.range([height, 0]); 
+
+    var yAxis = d3.svg.axis()
+	              .scale(y)
+	              .orient("left");
+
+	var remove_y = svg.selectAll("#y_axis")
+						.remove();
+
+	var draw_y = svg.append("g")
+				    .attr("class", "axis")
+				    .attr("id", "y_axis")
+				    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+				    .call(yAxis)
+				.append("text")
+					.attr("transform", "rotate(-90)")
+				    .attr("y", - 80)
+				    .attr("dy", ".8em")
+				    .style("text-anchor", "end")
+				    .text("Number of Slave Exports")
+        			.style("font-size", "13px")
+        			.style("font-weight", "bold");
+
+     // create rectangles       			
+	var rects = svg.selectAll(".bar")
+					.remove();
+
+	var rects = svg.selectAll(".bar").data(current_country)
+					.enter()
+					.append("rect")
+					.attr("class", "bar")
+					.attr("x", function(d, i) { return i * (width / 4) + margin.left + barPadding; })
+				    .attr("width", width / 4 - barPadding )
+				    .attr("y", function(d) { return y(d) + margin.top; })
+				    .attr("height", function(d) { return height - y(d); })
+				 	.on("mouseover", tip.show)
+					.on("mouseout", tip.hide);
+
+};
+
+
+
